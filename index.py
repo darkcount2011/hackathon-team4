30bac1fc364b4bbf478b3fe1d031608ad4b2123b
Auxority <gide0013@hz.nl>
2022-07-05 15:22:04 +0100

    working version

--- index.py
+++ index.py
@@ -1,21 +1,31 @@
-# from naoqi import ALProxy
-import naoqi
-# from dotenv import load_dotenv
+from naoqi import ALProxy
 
-# load_dotenv()
+# read file contents
+def read_file(file_name):
+    with open(file_name, 'r') as f:
+        return f.read()
 
-# pepper_ip = "172.16.4.85"
-# pepper_port = 9559
+# load variables from file
+def load_variables(file_name):
+    variables = {}
+    for line in read_file(file_name).splitlines():
+        if line.startswith('#'):
+            continue
+        if '=' not in line:
+            continue
+        k, v = line.split('=', 1)
+        variables[k] = v
+    return variables
 
-# tts = ALProxy("ALTextToSpeech", pepper_ip, pepper_port)
+VARIABLES = load_variables(".env")
+print(VARIABLES)
 
-# tts.say("Hello, world!")
+tts = ALProxy(
+    "ALTextToSpeech",
+    VARIABLES["PEPPER_IP"],
+    VARIABLES["PEPPER_PORT"]
+)
 
-# print("Hello world!")
+tts.say("Hello, world!")
 
-# # Robotic operating System - Tip van Daan
-
-# # p = new Pepper()
-
-# # p.forward(10)
-# # p.rotate(90)
+# Robotic operating System - Tip van Daan
