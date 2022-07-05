02bdad3f8498317aa696c7d2057f9e6a3a1a2445
Auxority <gide0013@hz.nl>
2022-07-05 13:32:59 +0100

    first test

--- index.py
+++ index.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+# from naoqi import ALProxy
+import naoqi
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# pepper_ip = "172.16.4.85"
+# pepper_port = 9559
+
+# tts = ALProxy("ALTextToSpeech", pepper_ip, pepper_port)
+
+# tts.say("Hello, world!")
+
+# print("Hello world!")
+
+# # Robotic operating System - Tip van Daan
+
+# # p = new Pepper()
+
+# # p.forward(10)
+# # p.rotate(90)
